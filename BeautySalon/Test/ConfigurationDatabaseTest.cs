b21d45bd34506b9e25ca9aa19ec918460f86af7f
DelphyAlmond <delphyalmond@gmail.com>
2025-05-26 12:37:43 +0400

    [!] BLCfix needed

--- BeautySalon/Test/ConfigurationDatabaseTest.cs
+++ BeautySalon/Test/ConfigurationDatabaseTest.cs
@@ -7,5 +7,3 @@ internal class ConfigurationDatabaseTest : IConfigurationDatabase
     public string ConnectionString =>
         "Server=127.0.0.7;Port=5472;Database=beautysalonDB;Uid=Del8a;Pwd=del8almond;";
 }
-
-// Can you make sure, the cashboxes are taken from DB and can you first of all check if connection with DB is actually working? And where it need to be stated, to connect the code. 
